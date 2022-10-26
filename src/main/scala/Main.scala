71f0ad7dfa71747e7824e02f1aa99b03e45eb524
calinanca99 <calinanca99@gmail.com>
2022-10-26 17:11:54 +0200

    Describe workflow

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,12 @@
-@main def hello: Unit = 
+@main def hello: Unit =
+
+  // Workflow
+
+  // 1. Initiate a Git repository in "../test-dir"
+  // 2. Create a text-file with "Hello, JGit!"
+  // 3. Add that file to staging
+  // 4. Write a commit message
+
   println("Hello world!")
   println(msg)
 
